d9b9a66ea308969d2e8f0fb745b2c547ac64b47d
Vigneshwaran-192124181 <112632219+Vigneshwaran-192124181@users.noreply.github.com>
2022-10-03 09:44:40 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,6 @@
+a=int(input("enter the no:-"))
+if(a//2):
+    print("it is a even no")
+else:
+     print(" odd number")
+   
---
+++ factorial.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number:"))
+fact=1
+for i in range(1,a+1):
+        factorial=factorial*i
+print("the factorial of given value:",fact)        
